043cfabdfd9d9f7ec3af24555cc885a80ec1e812
Maria Nevarez <mnevarez1220@gmail.com>
2020-09-30 11:25:55 -0600

    updated footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,55 +1,19 @@
 /* eslint-disable */
 import React from "react";
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
-// nodejs library that concatenates classes
-import classNames from "classnames";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-// import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// // @material-ui/icons
-// import Favorite from "@material-ui/icons/Favorite";
-// ../../assets/jss/material-kit-pro-react/components/footerStyle.js
-import styles from "../../assets/jss/material-kit-pro-react/components/cardFooterStyle";
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+// or
 
-const useStyles = makeStyles(styles);
+export default function Footer() {
 
-export default function Footer(props) {
-  const { children, content, theme, big, className } = props;
-  const classes = useStyles();
-  const themeType =
-    theme === "transparent" || theme == undefined ? false : true;
-  const footerClasses = classNames({
-    [classes.footer]: true,
-    [classes[theme]]: themeType,
-    [classes.big]: big || children !== undefined,
-    [className]: className !== undefined
-  });
-  const aClasses = classNames({
-    [classes.a]: true
-  });
+
 
   return (
-    <footer className={footerClasses}>
-      <div className={classes.container}>
-        {children !== undefined ? (
-          <div>
-            <div className={classes.content}>{children}</div>
-            <hr />
-          </div>
-        ) : (
-            " "
-          )}
-        {content}
-        <div className={classes.clearFix} />
-      </div>
-    </footer>
-  );
+    <BottomNavigation >
+      <BottomNavigationAction label="Recents" value="recents" />
+      <BottomNavigationAction label="Favorites" value="favorites" />
+      <BottomNavigationAction label="Nearby" value="nearby" />
+      <BottomNavigationAction label="Folder" value="folder" />
+    </BottomNavigation>
+  )
 }
-
-Footer.propTypes = {
-  theme: PropTypes.oneOf(["dark", "white", "transparent"]),
-  big: PropTypes.bool,
-  content: PropTypes.node.isRequired
-};
